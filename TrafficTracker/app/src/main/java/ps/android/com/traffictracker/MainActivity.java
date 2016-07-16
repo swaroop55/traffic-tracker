be1721d21f8c97b169d1801b9fb1435fa81ccb7e
swaroop55 <swaroopsanthu@gmail.com>
2016-07-16 20:20:54 +0530

    First Commit

---
+++ TrafficTracker/app/src/main/java/ps/android/com/traffictracker/MainActivity.java
@@ -0,0 +1,13 @@
+package ps.android.com.traffictracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
