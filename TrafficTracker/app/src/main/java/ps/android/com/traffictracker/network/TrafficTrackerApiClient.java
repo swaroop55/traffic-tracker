958598aa53e08e20283f0788c54cb770b26a1dc6
ps-swaroop <satyanarayana.p@flipkart.com>
2017-02-17 16:01:13 +0530

    Add logging

--- TrafficTracker/app/src/main/java/ps/android/com/traffictracker/network/TrafficTrackerApiClient.java
+++ TrafficTracker/app/src/main/java/ps/android/com/traffictracker/network/TrafficTrackerApiClient.java
@@ -1,5 +1,7 @@
 package ps.android.com.traffictracker.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,11 +16,17 @@ public class TrafficTrackerApiClient {
 
 
     public static Retrofit getClient() {
-        if (retrofit==null) {
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
         return retrofit;
-    }}
+    }
+}
